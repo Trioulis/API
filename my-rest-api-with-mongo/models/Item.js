cc50fceaab4cbfc0d71be58ab50e1e3fc591a0ff
Trioulis <vasileiostriantafyllidis3@gmail.com>
2024-09-27 13:29:25 +0300

    Deleted trial version

--- my-rest-api-with-mongo/models/Item.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const itemSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    quantity: Number
-});
-
-module.exports = mongoose.model('Item', itemSchema);
